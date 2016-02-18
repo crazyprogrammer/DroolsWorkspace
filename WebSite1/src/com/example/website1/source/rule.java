2a9c8e10f7799c5e7e690264ae9c87c3083acf35
Sudharshan Acharia <lovesaha2010@gmail.com>
2016-02-18 14:39:50 +0530

    changes with WebSite1

--- WebSite1/src/com/example/website1/source/rule.java
+++ WebSite1/src/com/example/website1/source/rule.java
@@ -7,25 +7,27 @@ import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
+import com.example.website1.bean.MediclaimProcessModel;
+
 public class rule {
 	
 	/**
 	 * Get set go!!!
 	 * @return 
 	 */
-	public void check (Object mpm) {
+	public void check (Object mpm, String Ruleconfig) {
 			try {  
 				// load up the knowledge base
 				KieServices ks = KieServices.Factory.get();
 	    	    KieContainer kContainer = ks.getKieClasspathContainer();
-	        	KieSession ksession = kContainer.newKieSession("medical-rules");
+	        	KieSession ksession = kContainer.newKieSession(Ruleconfig);
 				
 				// set the parameters
 				Map<String, Object> params = new HashMap<String, Object>();
 				
-				/*MediclaimProcessModel mpm = new MediclaimProcessModel();
+				//MediclaimProcessModel m = new MediclaimProcessModel();
 				
-				mpm.setUserName("Sudharshan");
+				/*mpm.setUserName("Sudharshan");
 				mpm.setUserAge(23);
 				mpm.setUserCoverage(true);*/
 				
